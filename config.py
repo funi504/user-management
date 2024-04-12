fa07109d3146e38f5d54849d171ce78ce4069725
funi504 <nekhungunifunanani9@gmail.com>
2024-04-12 17:31:10 +0200

    added database url

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import redis
+import os
 class ApplicationConfig:
     SECRET_KEY = "azjEMb$hfnvn@djcbvhf123cb4d"
 
     SQLACHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
-
+    
+    SQLALCHEMY_DATABASE_URI = r"postgres://database_user_management_database_user:nExgXpGz4OCvh3F13g8dfOhJXY3yP2Ov@dpg-cocl5ha1hbls73ct1hh0-a/database_user_management_database"
+    
     SESSION_TYPE ="redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_REDIS = redis.from_url(os.environ['REDIS_URL'])
