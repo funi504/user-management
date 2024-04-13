f223f7118605f623b278452f77faf0fd124c7989
funi504 <64310709+funi504@users.noreply.github.com>
2024-04-13 12:00:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ class ApplicationConfig:
 
     SQLACHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    if os.getenv('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    if os.environ.get('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
     else:
         SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
     
     SESSION_TYPE ="redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    if os.getenv('REDIS_URL'):
-        SESSION_REDIS = os.getenv('REDIS_URL')
+    if os.environ.get('REDIS_URL'):
+        SESSION_REDIS = os.environ.get('REDIS_URL')
     else:
         SESSION_REDIS = r"sqlite:///./db.sqlite"
